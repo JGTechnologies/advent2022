30dc86475a46e7c81f73cf5cf8758c565a72056f
Justin Gardner <justin@hirelofty.com>
2022-12-01 15:21:04 -0700

    Day 1 Part 2

--- src/day_1.rs
+++ src/day_1.rs
@@ -1,10 +1,17 @@
 use crate::helpers;
+use std::cmp::Reverse;
 
 struct Inventory {
     calories: Vec<u32>,
 }
 
-fn get_part_1_inputs() -> Vec<Inventory> {
+impl Inventory {
+    fn total_calories(&self) -> u32 {
+        self.calories.iter().sum()
+    }
+}
+
+fn get_inputs() -> Vec<Inventory> {
     let inputs = helpers::read_inputs_file(1)
         .into_iter();
 
@@ -27,21 +34,22 @@ fn get_part_1_inputs() -> Vec<Inventory> {
 }
 
 pub fn solve_part(part: u8) -> u32 {
+    let mut inventories = get_inputs();
+    let mut max: u32 = 0;
+
+    for inventory in &inventories {
+        let total = inventory.total_calories();
+
+        if total > max {
+            max = total;
+        }
+    }
+
+    inventories.sort_by_key(|inventory| Reverse(inventory.total_calories()));
+
     match part {
-        1 => {
-            let inventories = get_part_1_inputs();
-            let mut max: u32 = 0;
-
-            for inventory in inventories {
-                let total = inventory.calories.iter().map(|&item| item).sum::<u32>();
-
-                if total > max {
-                    max = total;
-                }
-            }
-
-            max
-        },
+        1 => inventories[0].total_calories(),
+        2 => inventories[0..3].iter().map(|inventory| inventory.total_calories()).sum(),
         _ => panic!("Invalid part number"),
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,5 @@ mod helpers;
 
 fn main() {
     println!("Day 1 - Part 1: {}", day_1::solve_part(1));
+    println!("Day 1 - Part 2: {}", day_1::solve_part(2));
 }
