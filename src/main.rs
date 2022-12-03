ac879b759ac214c3067342941e13e1b5e2f72901
Justin Gardner <gardner.justin@gmx.com>
2022-12-03 13:53:58 -0700

    Day 3 Part 1

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 mod helpers;
 mod day_1;
 mod day_2;
+mod day_3;
 
 fn main() {
     println!("Day 1 - Part 1: {}", day_1::solve_part(1));
     println!("Day 1 - Part 2: {}", day_1::solve_part(2));
     println!("Day 2 - Part 1: {}", day_2::solve_part(1));
     println!("Day 2 - Part 2: {}", day_2::solve_part(2));
+    println!("Day 3 - Part 1: {}", day_3::solve_part(1));
+    println!("Day 3 - Part 2: {}", day_3::solve_part(2));
 }
