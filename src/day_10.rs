b79554cf44bc70842b91c2bef65b141f77d0290d
Justin Gardner <gardner.justin@gmx.com>
2022-12-11 15:42:55 -0700

    Day 10 Part 2

--- src/day_10.rs
+++ src/day_10.rs
@@ -9,13 +9,13 @@ pub fn solve_part(part: u8) -> u32 {
   ]);
 
   for _i in 0..inputs.len() {
+    cycle += 1;
     let input = inputs[_i].clone();
-    let current_value = cycles.get_key_value(&(cycles.len() - 1)).unwrap().1;
+    let current_value = *cycles.get_key_value(&(cycles.len() - 1)).unwrap().1;
 
     if input == "noop" {
-      cycles.insert(cycle, *current_value);
+      cycles.insert(cycle, current_value);
 
-      cycle += 1;
       continue;
     }
 
@@ -23,12 +23,10 @@ pub fn solve_part(part: u8) -> u32 {
     splits.next();
     let value = splits.next().unwrap().parse::<i32>().unwrap();
 
-    let next = current_value + value;
+    cycles.insert(cycle, current_value);
+    cycle += 1;
 
-    cycles.insert(cycle, *current_value);
-    cycles.insert(cycle + 1, next);
-
-    cycle += 2;
+    cycles.insert(cycle, current_value + value);
   }
 
   match part {
@@ -37,12 +35,34 @@ pub fn solve_part(part: u8) -> u32 {
       let mut value: i32 = 0;
 
       for key in keys {
-        let x: i32 = (cycles.get_key_value(&(key as usize - 2)).unwrap().1 * key).try_into().unwrap();
+        let x: i32 = (cycles.get_key_value(&(key as usize - 1)).unwrap().1 * key).try_into().unwrap();
         value += x;
       }
 
       value as u32
     },
+    2 => {
+      let mut pixel: i32 = 0;
+
+      for row in 0..6 {
+        for col in 0..40 {
+          let position = row * 40 + col;
+          let sprite_center: i32 = *cycles.get_key_value(&position).unwrap().1;
+
+          if (sprite_center - (position as i32) % 40).abs() <= 1 {
+            print!("{}", '#');
+          } else {
+            print!("{}", '.');
+          }
+
+          pixel += 1;
+        }
+
+        println!("");
+      }
+
+      8
+    }
     _ => panic!("Invalid part number"),
   }
 }
