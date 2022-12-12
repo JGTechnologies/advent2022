da4e014f6e267b6b3b5e770e407d809d8b5f9b56
Justin Gardner <gardner.justin@gmx.com>
2022-12-11 23:52:38 -0700

    Day 11 Part 2

--- src/day_11.rs
+++ src/day_11.rs
@@ -10,23 +10,29 @@ enum OperationType {
 #[derive(Clone)]
 struct Monkey {
   id: usize,
-  items: Vec<u32>,
+  items: Vec<u64>,
   operation_type: OperationType,
-  operation_amount: u32,
-  test: u32,
+  operation_amount: u64,
+  test: u64,
   test_pass_target: u8,
   test_fail_target: u8,
-  items_inspected: u32,
+  items_inspected: u64,
 }
 
 impl Monkey {
-  fn execute_turn(&mut self, monkeys: &mut Vec<Monkey>) {
+  fn execute_turn(&mut self, monkeys: &mut Vec<Monkey>, divisor: u64) {
     for item in &self.items {
-      let worry_level = match &self.operation_type {
-        OperationType::ADD => (item + self.operation_amount) / 3,
-        OperationType::MULTIPLY => (item * self.operation_amount) / 3,
-        OperationType::POWER => item.pow(self.operation_amount.into()) / 3,
+      let mut worry_level = match &self.operation_type {
+        OperationType::ADD => item + self.operation_amount,
+        OperationType::MULTIPLY => item * self.operation_amount,
+        OperationType::POWER => item.pow(self.operation_amount as u32),
       };
+
+      if divisor == 3 {
+        worry_level /= 3;
+      } else {
+        worry_level %= divisor;
+      }
 
       monkeys[self.id].items_inspected += 1;
 
@@ -41,52 +47,7 @@ impl Monkey {
   }
 }
 
-pub fn solve_part(part: u8) -> u32 {
-  /*
-  let mut monkeys = vec![
-    Monkey {
-      id: 0,
-      items: vec![79, 98],
-      operation_type: OperationType::MULTIPLY,
-      operation_amount: 19,
-      test: 23,
-      test_pass_target: 2,
-      test_fail_target: 3,
-      items_inspected: 0,
-    },
-    Monkey {
-      id: 1,
-      items: vec![54, 65, 75, 74],
-      operation_type: OperationType::ADD,
-      operation_amount: 6,
-      test: 19,
-      test_pass_target: 2,
-      test_fail_target: 0,
-      items_inspected: 0,
-    },
-    Monkey {
-      id: 2,
-      items: vec![79, 60, 97],
-      operation_type: OperationType::POWER,
-      operation_amount: 2,
-      test: 13,
-      test_pass_target: 1,
-      test_fail_target: 3,
-      items_inspected: 0,
-    },
-    Monkey {
-      id: 3,
-      items: vec![74],
-      operation_type: OperationType::ADD,
-      operation_amount: 3,
-      test: 17,
-      test_pass_target: 0,
-      test_fail_target: 1,
-      items_inspected: 0,
-    },
-  ];
-  */
-
+pub fn solve_part(part: u8) -> u64 {
   let mut monkeys = vec![
     Monkey {
       id: 0,
@@ -170,23 +131,35 @@ pub fn solve_part(part: u8) -> u32 {
     },
   ];
 
-  let target_rounds = 20;
+  let target_rounds = match part {
+    1 => 20,
+    2 => 10000,
+    _ => panic!("Invalid part number"),
+  };
+
+  let divisor = match part {
+    1 => 3,
+    2 => {
+      let mut result = 1;
+
+      for monkey in &monkeys {
+        result *= monkey.test;
+      }
+
+      result
+    },
+    _ => panic!("Invalid part number"),
+  };
 
   for _round in 0..target_rounds {
     for i in 0..monkeys.clone().len() {
       let mut monkey = monkeys[i].clone();
 
-      monkey.execute_turn(&mut monkeys);
+      monkey.execute_turn(&mut monkeys, divisor);
     }
   }
 
-  match part {
-    1 => {
-      monkeys.sort_by_key(|m| Reverse(m.items_inspected));
+  monkeys.sort_by_key(|m| Reverse(m.items_inspected));
 
-      return (monkeys[0].items_inspected * monkeys[1].items_inspected).into();
-    },
-    2 => 0,
-    _ => panic!("Invalid part number"),
-  }
+  monkeys[0].items_inspected * monkeys[1].items_inspected
 }
